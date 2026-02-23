c782cb1101f6dd45339cda6f3c5cef8ae8a7500b
Krishnakantdwivedi2006 <godkrishnakant@gmail.com>
2026-02-23 14:06:06 +0530

    updated AppVehicle routes

--- routes/appVehicle.router.js
+++ routes/appVehicle.router.js
@@ -1,10 +1,10 @@
 import express from "express";
-import AppVehicleController from "../controller/appVehicle.controller";
+import AppVehicleController from "../controller/appVehicle.controller.js";
 
 const AppVehicleRoute = express.Router();
 
 AppVehicleRoute.post("/addVehicle", AppVehicleController.createVehicle);        // Admin
-router.get("/get-all-vehicle", AppVehicleController.getAllVehicles);         // Mobile App
+AppVehicleRoute.get("/get-all-vehicle", AppVehicleController.getAllVehicles);         // Mobile App
 AppVehicleRoute.get("/:id", AppVehicleController.getVehicleById);
 AppVehicleRoute.put("/:id", AppVehicleController.updateVehicle);       // Admin
 AppVehicleRoute.delete("/:id", AppVehicleController.deleteVehicle);    // Admin (soft delete)
