599678f203846e6d24fa815d9df6242cbe735a4d
Krishnakant Dwivedi <godkrishnakant@gmail.com>
2026-02-27 00:26:53 +0530

    corected twilio connection

--- connectons/connectTwilio.js
+++ connectons/connectTwilio.js
@@ -4,15 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Validate early (fail fast)
-if (!process.env.TWILIO_ACCOUNT_ID) throw new Error("Missing TWILIO_ACCOUNT_SID");
-if (!process.env.TWILIO_API_SECRET) throw new Error("Missing TWILIO_AUTH_TOKEN");
+if (!process.env.TWILIO_ACCOUNT_SID) throw new Error("Missing TWILIO_ACCOUNT_SID");
+if (!process.env.TWILIO_AUTH_TOKEN) throw new Error("Missing TWILIO_AUTH_TOKEN");
 if (!process.env.TWILIO_VERIFY_SERVICE_SID) throw new Error("Missing TWILIO_VERIFY_SERVICE_SID");
 
 const client = twilio(
-  process.env.TWILIO_ACCOUNT_ID,
-  process.env.TWILIO_API_SECRET
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
 );
 
-export const VERIFY_SERVICE_ID = process.env.TWILIO_VERIFY_SERVICE_ID;
+export const VERIFY_SERVICE_SID = process.env.TWILIO_VERIFY_SERVICE_SID;
 
 export default client;
